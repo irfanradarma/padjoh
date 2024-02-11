7e66e5b2aeaeaed810b3f0f1eb42802ecce2de58
irfanradarma <rajairfan.teuku@gmail.com>
2024-02-11 11:07:08 +0700

    tambah fitur

--- app.py
+++ app.py
@@ -10,12 +10,13 @@ st.set_page_config(layout="centered", page_title="Padjoh Journals")
 data_dummy = "https://docs.google.com/spreadsheets/d/1NtVj5P20IBdpNfClBhqW6egc8_atUn5VLf7_HdMC_tE/export?format=csv&gid=1161786680"
 data_real = "https://docs.google.com/spreadsheets/d/15HrkvSVfOakXgYKk-CIPFea0BivaEaQdxirQsvImvFk/export?format=csv&gid=1189388432"
 
-def load_cash():
+def load_cash(drop='T'):
     df = pd.read_csv(data_real)
     df = df.drop(['Timestamp'], axis=1)
     df['Jumlah'] = df['Jumlah'].astype(int)
     df['Tanggal Jurnal'] = pd.to_datetime(df['Tanggal Jurnal'])
-    df = df.drop(['Keterangan'], axis=1)
+    if drop == 'T':
+        df = df.drop(['Keterangan'], axis=1)
     return df
 
 def pivoting(df):
@@ -70,12 +71,13 @@ def main():
         period = st.radio("Show Daily/Monthly Recap", ["Daily", "Monthly"])
         if period == "Monthly":
             df_m = month_data(load_cash())
-            st.dataframe(df_m.drop(['Tanggal Jurnal'], axis=1))
+            st.dataframe(df_m.drop(['Tanggal Jurnal'], axis=1), hide_index=True)
             plot_monthly_data(df_m)
         if period == "Daily":
             df_d = daily_data(load_cash())
-            df_d['Tanggal Jurnal'] = df_d['Tanggal Jurnal'].dt.date
-            st.dataframe(df_d)
+            df_d['Tanggal Jurnal'] = df_d['Tanggal Jurnal'].dt.strftime('%b-%d')
+            df_d = df_d.rename(columns={'Tanggal Jurnal': 'Tanggal'})
+            st.dataframe(df_d, hide_index=True)
         st.link_button("Input Data Baru", "https://forms.gle/BATMEaJSGeY4TMkR7")
     with tab_custom:
         col1, col2 = st.columns(2)
@@ -83,6 +85,29 @@ def main():
             start = st.date_input("start date:", datetime.date(2024, 1, 1))
         with col2:
             end = st.date_input("end date", datetime.datetime.now())
+        df_d_custom = daily_data(load_cash())
+        df_d_custom['Tanggal Jurnal'] = df_d_custom['Tanggal Jurnal'].dt.date
+        df_d_custom = df_d_custom[(df_d_custom['Tanggal Jurnal'] >= start) & (df_d_custom['Tanggal Jurnal'] <= end)]
+        df_d_custom = df_d_custom.rename(columns={'Tanggal Jurnal': 'Tanggal'})
+        tab_income, tab_expense = st.tabs(["Pemasukan", "Pengeluaran"])
+        with tab_income:
+            df_custom = df_d_custom.copy()
+            df_custom = df_custom.drop(['Nett', 'Pengeluaran'], axis=1)
+            df_custom['Tanggal'] = pd.to_datetime(df_custom['Tanggal']).dt.strftime('%b-%d')
+            df_custom.sort_values(by='Tanggal', inplace=True)
+            st.dataframe(df_custom, hide_index=True)
+        with tab_expense:
+            df_out_custom = load_cash(drop='F')
+            df_out_custom = df_out_custom[df_out_custom['Jenis'] == 'Pengeluaran']
+            df_out_custom = df_out_custom[(pd.to_datetime(df_out_custom['Tanggal Jurnal']).dt.date >= start)
+                                          & (pd.to_datetime(df_out_custom['Tanggal Jurnal']).dt.date <= end)]
+            df_out_custom['Tanggal Jurnal'] = df_out_custom['Tanggal Jurnal'].dt.strftime('%b-%d')
+            df_out_custom = df_out_custom.rename(columns={'Tanggal Jurnal': 'Tanggal'})
+            df_out_custom = df_out_custom[['Tanggal', 'Keterangan', 'Jumlah']]
+            df_out_custom.sort_values(by='Tanggal', inplace=True)
+            st.dataframe(df_out_custom, hide_index=True)
+
+
         # st.link_button("Input Data Baru", "https://forms.gle/qpnVo48wLkURbYr77")
 
 if __name__ == "__main__":
