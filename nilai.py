27e6d6f43d6bf90e4c909d54586195727a9a7f25
Irfan Radarma <102050150+irfanradarma@users.noreply.github.com>
2024-12-30 10:04:47 +0700

    Update nilai.py

--- nilai.py
+++ nilai.py
@@ -7,5 +7,6 @@ st.title("Rekap Nilai Tugas Audit Sistem Informasi UMN")
 df = pd.read_csv("https://docs.google.com/spreadsheets/d/13GL9wjH-nT2qqsvMG2eYs8eMVZwEPADhKNaBArZDrec/export?format=csv&gid=2114973829")
 df['NIM'] = df['NIM'].astype(str)
 df['NIM'] = "000000"+df['NIM'].astype(str)
-df['Query SQL', 'SQL Drill', 'DBase Breach', 'Payroll', 'Travel Expense', 'Average'] = df['Query SQL', 'SQL Drill', 'DBase Breach', 'Payroll', 'Travel Expense', 'Average'].applymap(lambda x: f"{x:.2f}" if pd.notnull(x) else x)
+numeric_cols = ['Query SQL', 'SQL Drill', 'DBase Breach', 'Payroll', 'Travel Expense', 'Average']
+df[numeric_cols] = df[numeric_cols].applymap(lambda x: f"{x:.2f}" if pd.notnull(x) else x)
 st.dataframe(df, hide_index=True, use_container_width=True, height=5000)
